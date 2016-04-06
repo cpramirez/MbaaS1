a54a1513c40f18054e897e6cc21c5b9e24a54b15
CICE <cice@aula03mac01.local>
2016-04-06 18:17:19 +0200

    Guardado de ejemplo

--- HalCash/ViewController.swift
+++ HalCash/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //vamos a guardar unos datos de ejemplo (datos de un juego)
+        let gameScore = PFObject(className:"GameScore")
+        gameScore["score"] = 1337
+        gameScore["playerName"] = "Sean Plott"
+        gameScore["endDate"] = "06/04/2016"
+        gameScore.setObject("David Ricardo", forKey: "partner")
+        gameScore["enterprise"] = "HalCash"
+
+        gameScore.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError?) -> Void in
+            if (success) {
+                self.displayAlertViewController("HEY",messageString: "Datos guardados correctamente")
+            } else {
+                self.displayAlertViewController("HEY",messageString: "Información no guaradad \(error?.description)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: -  UTILS
+    func displayAlertViewController(titleString : String, messageString : String){
+        let alertVC = UIAlertController(title: titleString, message: messageString, preferredStyle: .Alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        presentViewController(alertVC, animated: true, completion: nil)
+    }
 
 }
 
